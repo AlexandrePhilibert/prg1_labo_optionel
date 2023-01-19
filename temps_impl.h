457c2744dc17e20362c0fda3422e8d5f32fbba84
Alexandre Philibert <alexandre.philibert@ik.me>
2023-01-19 17:23:47 +0100

    .

--- temps_impl.h
+++ temps_impl.h
@@ -1,4 +1,36 @@
 #ifndef PRG1_LABO_OPTIONEL_TEMPS_IMPL_H
 #define PRG1_LABO_OPTIONEL_TEMPS_IMPL_H
 
+#include "temps.h"
+
+template<typename T>
+T Temps<T>::getHeure() const {
+   return heure;
+}
+
+template<typename T>
+T Temps<T>::getMinute() const {
+   return minute;
+};
+
+template<typename T>
+T Temps<T>::getSeconde() const {
+   return seconde;
+};
+
+template<typename T>
+void Temps<T>::setHeures(T h) {
+   heure = h;
+};
+
+template<typename T>
+void Temps<T>::setMinute(T m) {
+   minute = m;
+};
+
+template<typename T>
+void Temps<T>::setSecondes(T s) {
+   seconde = s;
+};
+
 #endif //PRG1_LABO_OPTIONEL_TEMPS_IMPL_H
